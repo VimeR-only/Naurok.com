5ff3affe4cae33bc1d256bbba372f91fd9409079
-=VimeR=- <85843021+VimeR-only@users.noreply.github.com>
2024-01-16 19:27:48 +0200

    Add files via upload

---
+++ naurok.com/naurok.com/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Text.RegularExpressions;
+using naurok.com.models;
+using Newtonsoft.Json;
+
+namespace naurok.com
+{
+    internal class Program
+    {
+        static async Task<string> GetTestId(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string htmlCode = await client.GetStringAsync(url);
+                    string[] lines = htmlCode.Split('\n');
+
+                    Regex regTest = new Regex(@"ng-app=""([^""]*)""");
+
+                    foreach (var line in lines)
+                    {
+                        if (!regTest.Match(line).Success) continue;
+
+                        Regex regId = new Regex(@"init\(\d+,\s*(\d+),\s*\d+\)");
+
+                        Match Id = regId.Match(line);
+
+                        if (Id.Success)
+                        {
+                            return Id.Groups[1].Value;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Опомилка під час виконання HTTP-запиту: {e.Message}.");
+                    throw;
+                }
+            }
+            return "";
+        }
+
+        static async Task<List<object>> GetQuest(string testid)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string json = await client.GetStringAsync("https://naurok.com.ua/api2/test/sessions/" + testid);
+
+                    List<object> arrayQuest = new List<object>();
+                    Rootobject? quest = JsonConvert.DeserializeObject<Rootobject>(json);
+
+                    foreach (var data in quest.questions)
+                    {
+                        arrayQuest.Add(data.GetQuestion());
+
+                        foreach (var item in data.options)
+                        {
+                            arrayQuest.Add(item.GetQuestion());
+                        }
+                        arrayQuest.Add(" ");
+                    }
+
+                    return arrayQuest;
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Помилка під час виконання Get запиту JSON: {e.Message}.");
+                }
+            }
+            return null;
+        }
+
+        static void PrintQuest(List<object> quest)
+        {
+            foreach (var item in quest)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+        }
+
+        static async Task Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            string url;
+            string testId;
+
+            Console.WriteLine("Введіть URL теста");
+
+            url = Console.ReadLine();
+
+            testId = await GetTestId(url);
+
+            if (testId == "")
+            {
+                Console.WriteLine("Помилка ID тесту не знайдено.");
+                Console.ReadLine();
+                return;
+            }
+
+            List<object> questList = new List<object>();
+            questList = await GetQuest(testId);
+
+            if (questList == null)
+            {
+                Console.WriteLine("Помилка масив з питанями пустий.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(" ");
+
+            PrintQuest(questList);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ naurok.com/naurok.com/models/Quest.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.RegularExpressions;
+
+namespace naurok.com.models
+{
+    public class Rootobject
+    {
+        public Session session { get; set; }
+        public Settings settings { get; set; }
+        public Document document { get; set; }
+        public Question[] questions { get; set; }
+    }
+
+    public class Session
+    {
+        public int id { get; set; }
+        public string? uuid { get; set; }
+        public object? account_id { get; set; }
+        public object? latest_question { get; set; }
+        public int start_at { get; set; }
+        public int answers { get; set; }
+    }
+
+    public class Settings
+    {
+        public int shuffle_question { get; set; }
+        public int shuffle_options { get; set; }
+        public int show_answer { get; set; }
+        public int show_review { get; set; }
+        public int show_leaderbord { get; set; }
+        public int show_memes { get; set; }
+    }
+
+    public class Document
+    {
+        public int questions { get; set; }
+    }
+
+    public class Question
+    {
+        public string id { get; set; }
+        public string content { get; set; }
+        public string image { get; set; }
+        public string type { get; set; }
+        public string point { get; set; }
+        public string hint { get; set; }
+        public string hint_penalty { get; set; }
+        public string hint_description { get; set; }
+        public string order { get; set; }
+        public object clone_id { get; set; }
+        public Option[] options { get; set; }
+        public int questionLength { get; set; }
+        public int optionMaxLength { get; set; }
+        public bool optionHasImage { get; set; }
+
+        public void PrintQuest()
+        {
+            foreach (var item in options)
+            {
+                Console.WriteLine($"{item.GetQuestion()}");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public string GetQuestion()
+        {
+            Regex p = new Regex(@"<p>(.*?)<\/p>");
+            Regex strong = new Regex(@"<\/?strong>");
+
+            return strong.Replace(p.Replace(content, "$1"), "");
+        }
+    }
+
+    public class Option
+    {
+        public string id { get; set; }
+        public string question_id { get; set; }
+        public string value { get; set; }
+        public string image { get; set; }
+        public object order { get; set; }
+
+        public string GetQuestion()
+        {
+            Regex p = new Regex(@"<p>(.*?)<\/p>");
+            Regex strong = new Regex(@"<\/?strong>");
+
+            return strong.Replace(p.Replace(value, "$1"), "");
+        }
+    }
+
+}
